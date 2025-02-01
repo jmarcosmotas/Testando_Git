ea1a38d3538d478518210b0a53a6e8fc6e494e6a
Joao Marcos <joaomarcosms97@outlook.com>
2025-02-01 20:46:40 -0300

    2º commit - lista encadeada

---
+++ Lista_encadeada.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+   int id; 
+   char nome[20];
+   int idade;
+}DADOS;
+
+typedef struct NO{
+    DADOS dados;
+    struct NO *next;
+}NO;
+
+void inserir(NO **lista,DADOS dados){
+    NO *novo =(NO*)malloc(sizeof(NO));
+    novo->dados = dados;
+    novo->next=NULL;
+    if(*lista == NULL){
+        *lista = novo;
+        return;
+    }
+    NO *atual = *lista;
+    while(atual->next){
+        atual=atual->next;
+    }
+    atual->next=novo;
+}
+void imprimir(NO *lista){
+    NO *atual=lista;
+    while(lista){
+        printf("%s %d\n",atual->dados.nome,atual->dados.idade);
+        atual=atual->next;
+    }
+}
+int main(){
+    NO *lista=NULL;
+    DADOS dados;
+    
+    strcpy(dados.nome,"Joao");
+    dados.idade=20;
+    inserir(&lista,dados);
+    strcpy(dados.nome,"Marcos");
+    dados.idade=23;
+    inserir(&lista,dados);
+    imprimir(lista);
+
+    return 0;
+}
